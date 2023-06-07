85db4718603fa2154b25becdad4e5e305d798a3a
manokrish2000 <manokrish104525@gmail.com>
2023-06-07 19:53:09 +0530

    cors

--- app.js
+++ app.js
@@ -11,6 +11,20 @@ const auth = require("./auth");
 
 dbConnect();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+
 
 // body parser configuration
 app.use(bodyParser.json());
