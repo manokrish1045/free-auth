3a20bce10b6a62c9a9d25c5487ff0be885a8c109
manokrish2000 <manokrish104525@gmail.com>
2023-06-12 09:30:07 +0530

    register

--- app.js
+++ app.js
@@ -38,45 +38,66 @@ app.get("/", (request, response, next) => {
   next();
 });
 
-app.post("/register", (request, response) => {
-  // hash the password
-  bcrypt
-    .hash(request.body.password, 10)
-    .then((hashedPassword) => {
-      // create a new user instance and collect the data
-      const user = new User({
-        email: request.body.email,
-        password: hashedPassword,
-      });
+// app.post("/register", (request, response) => {
+//   // hash the password
+//   bcrypt
+//     .hash(request.body.password, 10)
+//     .then((hashedPassword) => {
+//       // create a new user instance and collect the data
+//       const user = new User({
+//         email: request.body.email,
+//         password: hashedPassword,
+//       });
 
-      // save the new user
-      user.save()
-        // return success if the new user is added to the database successfully
-        .then((result) => {
-          response.status(201).send({
-            message: "User Created Successfully",
-            result,
-          });
-        })
-        // catch error if the new user wasn't added successfully to the database
-        .catch((error) => {
-          console.log(error)
-          response.status(400).send({
-            message: "Error creating user",
-            error,
+//       // save the new user
+//       user.save()
+//         // return success if the new user is added to the database successfully
+//         .then((result) => {
+//           response.status(201).send({
+//             message: "User Created Successfully",
+//             result,
+//           });
+//         })
+//         // catch error if the new user wasn't added successfully to the database
+//         .catch((error) => {
+//           console.log(error)
+//           response.status(400).send({
+//             message: "Error creating user",
+//             error,
 
-          });
-        });
-    })
-    // catch error if the password hash isn't successful
-    .catch((e) => {
-      console.log(e)
+//           });
+//         });
+//     })
+//     // catch error if the password hash isn't successful
+//     .catch((e) => {
+//       console.log(e)
 
-      response.status(500).send({
-        message: "Password was not hashed successfully",
-        e,
-      });
+//       response.status(500).send({
+//         message: "Password was not hashed successfully",
+//         e,
+//       });
+//     });
+// });
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+    const hashedpassword = await bcrypt.hash(password, 10);
+    user = new User({
+      name,
+      email,
+      password: hashedpassword,
     });
+    await user.save();
+    return res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    // console.log(err);
+    return res.status(400).json({ error: err.message });
+  }
 });
 
 
